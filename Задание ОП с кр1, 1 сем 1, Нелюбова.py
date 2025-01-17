4f9127b37b9ae40870ee851a779e55c954ff6dc2
nelvickus <145201653+nelvickus@users.noreply.github.com>
2025-01-17 21:04:43 +0300

    Update Задание ОП с кр1, 1 сем 1, Нелюбова.py

--- Задание ОП с кр1, 1 сем 1, Нелюбова.py
+++ Задание ОП с кр1, 1 сем 1, Нелюбова.py
@@ -6,7 +6,6 @@ def a(A, B):
         for num in range(A, B - 1, -1):
             print(num)
 
-# Пример использования функции
 A = int(input("Введите целое число A: "))
 B = int(input("Введите целое число B: "))
 a(A, B)
