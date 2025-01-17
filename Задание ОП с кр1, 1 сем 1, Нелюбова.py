30ee427b8f22be9395d9adf3509646eee34b057f
nelvickus <145201653+nelvickus@users.noreply.github.com>
2025-01-17 20:18:19 +0300

    Задание ОП с кр1, 1 сем 1, Нелюбова.py
    Вариант 2. 
    Даны два целых числа A и В. Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.

---
+++ Задание ОП с кр1, 1 сем 1, Нелюбова.py
@@ -0,0 +1,11 @@
+def a(A, B):
+    if A < B:
+        for num in range(A, B + 1):
+            print(num)
+    else:
+        for num in range(A, B - 1, -1):
+            print(num)
+          
+A = int(input("Введите целое число A: "))
+B = int(input("Введите целое число B: "))
+a(A, B)
